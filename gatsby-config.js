ff69c8305314918b7506a31849d28b2a43a4924e
Suraj <Surajdoberiyal@gmail.com>
2021-06-22 22:02:26 +0530

    Intial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ChandrikaDeb',
-        short_name: 'ChandrikaDeb',
+        name: 'Surajdoberiyal',
+        short_name: 'Surajdoberiyal',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
